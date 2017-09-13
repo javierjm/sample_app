7d6d820017d69c4be3f258e753793b525109c134
Javier Jara <javier.jara@logn.co>
2017-09-13 15:26:50 -0600

    bug fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,7 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  .
-  
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
