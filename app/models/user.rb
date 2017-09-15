1a0c3fe0839f2ef496d0af8e97b9239c74f54b84
Javier <javier.jara@logn.co>
2017-09-15 16:00:40 -0600

    12.3 Resetting the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
 	before_save :downcase_email
   before_create :create_activation_digest
@@ -11,22 +11,22 @@ class User < ApplicationRecord
 	has_secure_password
 
   # Returns the hash digest of the given string.
-	 def User.digest(string)
-    	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    	BCrypt::Password.create(string, cost: cost)
-  	end  
+  def User.digest(string)
+  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                BCrypt::Engine.cost
+  	BCrypt::Password.create(string, cost: cost)
+	end  
 
-  	# Returns a random token
-  	def User.new_token 
-  		SecureRandom.urlsafe_base64
-  	end
+	# Returns a random token
+	def User.new_token 
+		SecureRandom.urlsafe_base64
+	end
 
-  # Remembers a user in the database for use in persistent sessions.
-  	def remember
-  		self.remember_token = User.new_token 
-  		update_attribute(:remember_digest, User.digest(remember_token))
-  	end
+# Remembers a user in the database for use in persistent sessions.
+	def remember
+		self.remember_token = User.new_token 
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
 
   def activate 
     update_columns(activated: true, activated_at:Time.zone.now)
@@ -36,22 +36,34 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-# Previous specific to remember_token authenticated? method 
-#def authenticated?(remember_token)
-#  return false if remember_digest.nil?
-#  BCrypt::Password.new(remember_digest).is_password?(remember_token)
-#end
+  # Forgot Password machinery 
 
-  	def authenticated?(attribute, token)
-      digest = self.send("#{attribute}_digest")
-      
-      return false if digest.nil?
-    	BCrypt::Password.new(digest).is_password?(token)
-  	end
+  def create_reset_digest 
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
-  	def forget
-  		update_attribute(:remember_digest, nil)
-  	end
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Previous specific to remember_token authenticated? method 
+  #def authenticated?(remember_token)
+  #  return false if remember_digest.nil?
+  #  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  #end
+
+	def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    
+    return false if digest.nil?
+  	BCrypt::Password.new(digest).is_password?(token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
 
     private 
 
