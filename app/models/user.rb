d9dd11258a156ed299c3f6d974184f4a50e71723
Javier Jara <javier.jara@logn.co>
2017-09-18 16:33:45 -0600

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-class User < ApplicationRecord
+  class User < ApplicationRecord
+    
   attr_accessor :remember_token, :activation_token, :reset_token
 
 	before_save :downcase_email
@@ -40,12 +41,16 @@ class User < ApplicationRecord
 
   def create_reset_digest 
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  
   end
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired? 
+    return reset_sent_at < 2.hours.ago
   end
 
   # Previous specific to remember_token authenticated? method 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914232510) do
+ActiveRecord::Schema.define(version: 20170918174108) do
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type '' for column 'reset_digest'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
