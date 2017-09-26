2a042dfdacfe95f33369791b68efa0f578cfc7ee
Javier <javier.jara@logn.co>
2017-09-26 14:28:22 -0600

    bug fixing

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -13,7 +13,7 @@ class RelationshipsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "destroy should require logged-in user" do 
-  	assert_no_difference "Relaionship.count" do 
+  	assert_no_difference "Relationship.count" do 
   		delete relationship_path(relationships(:one))
   	end
   	assert_redirected_to login_url
